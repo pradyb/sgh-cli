4f19771d766538df08c8a8ae297c3c852306e523
Pradeep Kumar Balakrishnan <pradeep.devlabs@gmail.com>
2024-11-15 13:25:16 +0530

    Added PB add/remove user command help text

--- cmd/protected_branch/protected_branch.go
+++ cmd/protected_branch/protected_branch.go
@@ -72,6 +72,8 @@ func UpdateCommand(ctx *context.Context) *cobra.Command {
 			$ sgh pb update --org sample-org --branch sample-branch 
 			$ sgh pb update --org sample-org --branch sample-branch -r sample-repo1 -r sample-repo2
 			$ sgh pb update --org sample-org --branch sample-branch -r sample-repo1 -l -d
+			$ sgh pb update --org sample-org --branch sample-branch -r sample-repo1 -l -d -a john-doe -a jane-doe
+			$ sgh pb update --org sample-org --branch sample-branch -r sample-repo1 -l -d -u john-doe -u jane-doe
 		`),
 		Run: func(cmd *cobra.Command, args []string) {
 			orgName, _ := cmd.Flags().GetString("org")
